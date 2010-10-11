3f9bb1470c96a08b8e8c371d06c27c5b337fb8e9
unknown <zhaojie@.SNDA.ROOT.CORP>
2010-10-11 17:13:15 +0800

    first commit

---
+++ samples/SimpleConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Json;
+
+namespace SimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = "{\"v\":{\"Name\":\"Default\",\"Author\":{\"Name\":\"Jerry\",\"Age\":15},\"Posts\":[{\"Title\":\"Post 1\",\"CreateTime\":\"Fri, 01 Jan 2010 00:00:00 GMT\"},{\"Title\":\"Post 2\",\"CreateTime\":\"Mon, 01 Feb 2010 00:00:00 GMT\"},{\"Title\":\"Post 3\",\"CreateTime\":\"Mon, 01 Mar 2010 00:00:00 GMT\"}]}}";
+            var json = JsonValue.Parse(jsonString);
+        }
+    }
+}
---
+++ src/System.Json/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// Assembly System.Json, Version 2.0.5.0
+
+[assembly: System.Reflection.AssemblyVersion("2.0.5.0")]
+[assembly: System.Reflection.AssemblyDescription("System.Json.dll")]
+[assembly: System.Reflection.AssemblyDefaultAlias("System.Json.dll")]
+[assembly: System.Reflection.AssemblyCompany("MONO development team")]
+[assembly: System.Reflection.AssemblyProduct("MONO Common language infrastructure")]
+[assembly: System.Reflection.AssemblyCopyright("(c) various MONO Authors")]
+[assembly: System.Resources.SatelliteContractVersion("2.0.5.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.0.40818.0")]
+[assembly: System.Reflection.AssemblyFileVersion("3.0.40818.0")]
+[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(System.Runtime.CompilerServices.CompilationRelaxations.NoStringInterning)]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Security.SecurityCritical]
+[assembly: System.Reflection.AssemblyTitle("System.Json.dll")]
+
